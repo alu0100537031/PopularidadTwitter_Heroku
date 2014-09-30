0f6096d692d8fc9efebd9bf0e1819dfd71358b3a
Rushil Lakhani <rushil@rushil-VirtualBox.(none)>
2014-09-30 23:07:50 +0100

    Creando fichero test configurado

---
+++ test/test.rb
@@ -0,0 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+#require_relative '../twitter.rb'
+require_relative 'twitter.rb'
+
+
+include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
