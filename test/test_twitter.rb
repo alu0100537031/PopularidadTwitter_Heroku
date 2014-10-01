aa57ab5105079b235cafa4c09c9d678db3d8fa3d
Rushil Lakhani <rushil@rushil-VirtualBox.(none)>
2014-10-01 20:36:26 +0100

    Corrigiendo tests

---
+++ test/test_twitter.rb
@@ -0,0 +1,33 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'rack/test'
+require_relative '../twitter.rb'
+
+
+
+include Rack::Test::Methods
+	
+	def app
+		Sinatra::Application
+	end
+	
+describe "SYTW Práctica2-Heroku" do
+  
+
+  it "Should return index" do
+	get '/'
+	assert last_response.ok?
+  end  	
+  
+
+  it "should return title" do
+	get '/'
+	assert_match "<title>Popularidad en Twitter</title>", last_response.body
+  end
+  
+end
+	
+	
+	
+	
